7954ccca2e2805d928648e98efd969044a13d8d6
Hossam Abosaleh <hossamabosaleh7@gmail.com>
2022-08-03 16:44:49 +0200

    Initial Commit

---
+++ Muslim App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Muslim App
+//
+//  Created by Abo Saleh on 03/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
